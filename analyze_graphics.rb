afc63e11db843b08c263efa9aec180fd18c0c395
Bernard Potocki <bernard.potocki@imanel.org>
2011-03-02 09:12:52 +0100

    Formatting

--- analyze_graphics.rb
+++ analyze_graphics.rb
@@ -17,75 +17,75 @@ require 'rexml/xpath'
 #    You should have received a copy of the GNU Lesser General Public
 #    License along with this library; if not, write to the Free Software
 #    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-#	
-#	Author: J. David Eisenberg
-#	Contact: catcode@catcode.com
+#
+# Author: J. David Eisenberg
+# Contact: catcode@catcode.com
 
 class ODT_to_XHTML
 
-	def process_draw_frame( element, output_node )
-		style_name = register_style( element );
-		div = emit_element( output_node, "div", {"class" => style_name} )
-		attr = element.attribute("#{@svg_ns}:width")
-		if (attr != nil) then
-			modify_style_attribute( div, "width", attr.value )
-		end
-		attr = element.attribute("#{@svg_ns}:height")
-		if (attr != nil) then
-			modify_style_attribute( div, "height", attr.value )
-		end
-		process_children( element, div )
-	end
-	
-	#
-	#	Copy an image into user-specified directory, and emit
-	#	a corresponding <tt>&lt;img&gt;</tt> element.
-	#
-	#	If the user has not specified an image directory,
-	#	then emit a <tt>&lt;div&gt;</tt> containing the
-	#	file name.
-	def process_draw_image( element, output_node )
-		pic_name = element.attribute("#{@xlink_ns}:href").value
-		if (@image_dir != nil) then
-			img = emit_element( output_node, "img" )
-			img.attributes["alt"] = pic_name
-			
-			#	Get rid of everything before the last / in the filename
-			base_name = pic_name;
-			if ((pos = base_name.rindex('/')) != nil) then
-				base_name = base_name[pos + 1 .. -1]
-			end
-			copy_image_file( pic_name, @image_dir, base_name )
-			img.attributes["src"] = "#{@image_dir}/#{base_name}"
-			width = element.parent.attribute("#{@svg_ns}:width")
-			height= element.parent.attribute("#{@svg_ns}:height")
-			if (width != nil && height != nil) then
-				img.attributes["style"] = "width:#{width.value}; " + 
-					"height:#{height.value}"
-			end
-		else
-			div = emit_element( output_node, "div" )
-			div.add_text( pic_name )
-		end
-	end
-	
-	def copy_image_file( pic_name, directory, filename )
-		zipfile = Zip::ZipFile::open( @input_filename )
-		inStream = zipfile.get_entry( pic_name )
-		if (inStream != nil) then
-			inStream = inStream.get_input_stream
-			outStream =  File.new("#{directory}#{File::SEPARATOR}#{filename}", "w")
-			outStream.binmode
-			buf =  inStream.read
-			outStream.print buf
-			outStream.close
-			inStream.close
-		end
-		zipfile.close
-		rescue Exception => e
-			#
-			#	Uncomment next line if you want error output
-			# $stderr.puts "Could not find image #{pic_name}" 
-	end
+  def process_draw_frame( element, output_node )
+    style_name = register_style( element );
+    div = emit_element( output_node, "div", {"class" => style_name} )
+    attr = element.attribute("#{@svg_ns}:width")
+    if (attr != nil) then
+      modify_style_attribute( div, "width", attr.value )
+    end
+    attr = element.attribute("#{@svg_ns}:height")
+    if (attr != nil) then
+      modify_style_attribute( div, "height", attr.value )
+    end
+    process_children( element, div )
+  end
+
+  #
+  # Copy an image into user-specified directory, and emit
+  # a corresponding <tt>&lt;img&gt;</tt> element.
+  #
+  # If the user has not specified an image directory,
+  # then emit a <tt>&lt;div&gt;</tt> containing the
+  # file name.
+  def process_draw_image( element, output_node )
+    pic_name = element.attribute("#{@xlink_ns}:href").value
+    if (@image_dir != nil) then
+      img = emit_element( output_node, "img" )
+      img.attributes["alt"] = pic_name
+
+      # Get rid of everything before the last / in the filename
+      base_name = pic_name;
+      if ((pos = base_name.rindex('/')) != nil) then
+        base_name = base_name[pos + 1 .. -1]
+      end
+      copy_image_file( pic_name, @image_dir, base_name )
+      img.attributes["src"] = "#{@image_dir}/#{base_name}"
+      width = element.parent.attribute("#{@svg_ns}:width")
+      height= element.parent.attribute("#{@svg_ns}:height")
+      if (width != nil && height != nil) then
+        img.attributes["style"] = "width:#{width.value}; " +
+          "height:#{height.value}"
+      end
+    else
+      div = emit_element( output_node, "div" )
+      div.add_text( pic_name )
+    end
+  end
+
+  def copy_image_file( pic_name, directory, filename )
+    zipfile = Zip::ZipFile::open( @input_filename )
+    inStream = zipfile.get_entry( pic_name )
+    if (inStream != nil) then
+      inStream = inStream.get_input_stream
+      outStream =  File.new("#{directory}#{File::SEPARATOR}#{filename}", "w")
+      outStream.binmode
+      buf =  inStream.read
+      outStream.print buf
+      outStream.close
+      inStream.close
+    end
+    zipfile.close
+    rescue Exception => e
+      #
+      # Uncomment next line if you want error output
+      # $stderr.puts "Could not find image #{pic_name}"
+  end
 end
 
--- style_classes.rb
+++ style_classes.rb
@@ -14,24 +14,24 @@
 #    You should have received a copy of the GNU Lesser General Public
 #    License along with this library; if not, write to the Free Software
 #    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-#	
-#	Author: J. David Eisenberg
-#	Contact: catcode@catcode.com
+#
+# Author: J. David Eisenberg
+# Contact: catcode@catcode.com
 
 =begin rdoc
 This class represents a CSS declaration; a
 property/value pair
 =end
 class Declaration
-	attr_accessor( :property, :value )
-	def initialize( property=nil, value=nil )
-		@property = property
-		@value = value
-	end
+  attr_accessor( :property, :value )
+  def initialize( property=nil, value=nil )
+    @property = property
+    @value = value
+  end
 
-	def to_s
-		return "#{property}: #{value}"
-	end
+  def to_s
+    return "#{property}: #{value}"
+  end
 end
 
 =begin rdoc
@@ -39,33 +39,33 @@ Represents a CSS declaration block; a sequence of zero
 or more +Declaration+s.
 =end
 class DeclarationBlock < Array
-	attr_accessor( :block_used )
-	
-	def initialize(*arglist)
-		if (arglist[0].kind_of? DeclarationBlock) then
-			dblock = arglist[0]
-			super( 0 )
-			dblock.each do |item|
-				push Declaration.new( item.property, item.value )
-			end
-		else
-			super
-		end
-		@block_used = false
-	end
+  attr_accessor( :block_used )
 
-	def has_top_border?
-	result = detect {|item| item.property =~ /border(-top)?/}
-		return (result != nil) ? true : nil
-	end
+  def initialize(*arglist)
+    if (arglist[0].kind_of? DeclarationBlock) then
+      dblock = arglist[0]
+      super( 0 )
+      dblock.each do |item|
+        push Declaration.new( item.property, item.value )
+      end
+    else
+      super
+    end
+    @block_used = false
+  end
 
-	def to_s
-		result = "{\n"
-		each { |item|
-			result << "\t#{item.property}: #{item.value};\n"
-		}
-		result << "}\n"
-		return result
-	end
-		
+  def has_top_border?
+  result = detect {|item| item.property =~ /border(-top)?/}
+    return (result != nil) ? true : nil
+  end
+
+  def to_s
+    result = "{\n"
+    each { |item|
+      result << "\t#{item.property}: #{item.value};\n"
+    }
+    result << "}\n"
+    return result
+  end
+
 end
